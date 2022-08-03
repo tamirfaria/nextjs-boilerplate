4967c9423cdfd3269f7adbd55e954551d030765d
Lucas Tamir Faria <lucastamir@gmail.com>
2022-08-03 17:14:22 -0300

    refactor: atualizando testes

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,14 +1,25 @@
 import { render, screen } from '@testing-library/react'
 import Main from '.'
 
-describe('<Main />', () => {
-  it('should render the heading', () => {
+describe('<Main/>', () => {
+  it('should render Main without break', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /boilerplate nextjs/i })
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', {
+        name: /typescript, reactjs, nextjs e styled components/i
+      })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the background color correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
