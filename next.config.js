c42e1df2d084d0fcd157418549b8b98ddc48c063
Lucas Tamir Faria <lucastamir@gmail.com>
2022-09-30 18:04:29 -0300

    fix: corrigindo configuração do next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: !isDev
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  disable: !isProd,
+  register: true
 })
-const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disabled: !isProd
-  }
-})
+module.exports = withPWA(nextConfig)
