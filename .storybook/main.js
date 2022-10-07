df0611fbc480edadb2dd1629c82cd65006eae5ea
Lucas Tamir Faria <lucastamir@gmail.com>
2022-10-07 14:30:23 -0300

    chore: configurando build do storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   "stories": [ "../src/components/**/stories.tsx" ],
   "addons": [ "@storybook/addon-essentials" ],
+  babel: async (options) => {
+    return ({
+    ...options,
+    plugins: [...options.plugins, require.resolve('@babel/plugin-transform-react-jsx')]
+  })},
 }
